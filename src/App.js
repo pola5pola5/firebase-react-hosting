6eb924f17620fa2aaaf4b9027476ca0ae1754926
pola5pola5 <l.c.a.m.ayn@gmail.com>
2021-11-19 15:02:54 +0900

    完了，未完了ステータス追加

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
     }
     //todoを管理するための関数
     const addTodo = text => {
-        const newTodos = [...todos, text] //今までのデータ(...todos)と，追加したデータ(text)を配列に格納
+        const newTodos = [...todos, {text, complete: false}] // objectにしてステータスを追加
+        // const newTodos = [...todos, text] //今までのデータ(...todos)と，追加したデータ(text)を配列に格納
         setTodos(newTodos) // 配列のステートに保存
         console.log(newTodos)
     }
@@ -26,6 +27,13 @@ function App() {
         newTodos.splice(index, 1)
         setTodos(newTodos)
     }
+    const completeTodo = index => {
+        const newTodos = [...todos]
+        newTodos[index].complete = !newTodos[index].complete
+        setTodos(newTodos)
+        console.log(newTodos)
+    }
+
     return (
         <div className="App">
             <Container>
@@ -44,10 +52,14 @@ function App() {
                     <tbody>
                     {todos && todos.map((todo, index) => (
                         <tr key={index}>
-                            <td align={"left"}>
-                                {todo}
+                            <td align={"left"} class={todo.complete ? "text-secondary" : ""}>
+                                {todo.text}
                             </td>
                             <td align={"right"}>
+                                <Button color={todo.complete ? "secondary" : "success"}
+                                        onClick={() => completeTodo(index)}>
+                                    {todo.complete ? "完了" : "未完了"}
+                                </Button>
                                 <Button color={"danger"} onClick={() => removeTodo(index)}>削除</Button>
                             </td>
                         </tr>
